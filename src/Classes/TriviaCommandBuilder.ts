e56b4fc910d9895cdf80756202f8f3d52e48792f
TurtlePaw <turtlesimsyt@gmail.com>
2022-03-15 19:56:05 -0400

    📋 toData() -> toJSON() and no options required!

--- src/Classes/TriviaCommandBuilder.ts
+++ src/Classes/TriviaCommandBuilder.ts
@@ -11,12 +11,13 @@ import TriviaGame from "./TriviaGame";
 export default class TriviaCommandBuilder {
   private build!: SlashCommandBuilder;
   public gameOptions!: TriviaGameOptions;
-  private lockedGameOptionsData!: LockedGameOptionsEntry[];
+  private lockedGameOptionsData: LockedGameOptionsEntry[] = [];
+  private isApplied: Boolean = false;
 
-  constructor(options: TriviaCommandBuilderOptions) {
+  constructor(options?: TriviaCommandBuilderOptions) {
     this.build = new SlashCommandBuilder()
-      .setName(options.name)
-      .setDescription(options.description);
+      .setName(options?.name ?? "trivia")
+      .setDescription(options?.description ?? "Create a trivia game.");
   }
 
   private optionApplicators: LockedOptionApplier = {
@@ -176,13 +177,19 @@ export default class TriviaCommandBuilder {
   }
 
   // Returns incompatible with RESTPostAPIApplicationCommandsJSONBody
-  toData() {
-    this.applyOptions();
+  toJSON() { //If its toData() users command handlers probably will use toJSON() so it will throw an error
+    if(!this.isApplied){ 
+      this.applyOptions();
+      this.isApplied = true;
+    }
     return this.build.toJSON();
   }
 
   toBuilder() {
-    this.applyOptions();
+    if(!this.isApplied){ 
+      this.applyOptions();
+      this.isApplied = true;
+    }
     return this.build;
   }
 }
